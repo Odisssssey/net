ad283de42709c74fdabfffb52242ecd7ed83e824
odisssssey <good_man5@mail.ru>
2016-06-26 15:27:42 +0300

    <p>

--- about.php
+++ about.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
         <style>
             .new1{
+                padding: 0px 5px;
 				width: 100%;
 				height: auto;	
 				}
 			.new2{
+			    padding: 0px 10px;
 				width: auto;
 				height: auto;
 				float: left;				
@@ -16,11 +18,11 @@
 </head>
 <body>
 				<div class="new1" ><h1>About myself.</h1></div>
-            <div class="new2" ><a> <table><td>Имя</td></table> 
-                                   <table><td>Возраст</td></table>
-                                   <table><td>Адрес электронной почты</td></table>
-                                   <table><td>Город</td></table>
-                                   <table><td>О себе</td></table></a>
+            <div class="new2" ><a><p>Имя</p> 
+                                   <p>Возраст</p>
+                                   <p>Адрес электронной почты</p>
+                                   <p>Город</p>
+                                   <p>О себе</p>
             </div>
             <div class="new2" >
 			<?php
@@ -32,11 +34,11 @@
         
         
         
-            echo '<table><td>'.$name.'</td></table>';
-            echo '<table><td>'.$age.'</td></table>';
-            echo '<table><td>'.$mail.'</td></table>';
-            echo '<table><td>'.$city.'</td></table>';
-            echo '<table><td>'.$aboutys.'</td></table>';
+            echo "<p>$name</p>";
+            echo "<p>$age</p>";
+            echo "<p>$mail</p>";
+            echo "<p>$city</p>";
+            echo "<p>$aboutys</p>";
 
            
 
