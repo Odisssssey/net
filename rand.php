8f03e5a39ba22a67918d06f6339d13e0144fd343
odisssssey <good_man5@mail.ru>
2016-07-05 18:01:48 +0300

    rand

---
+++ rand.php
@@ -0,0 +1,86 @@
+﻿<html>
+  <head>
+  </head>
+  <body>
+  <h1>Угадай задуманное число за 30 секунд!</h1>
+    <p>
+      <?php
+		function random($min, $max)
+        {
+            $n = rand($min, $max);
+			setcookie('n', $n, time() + 35);
+            return $n;
+			
+        
+    
+        }
+		function sravnenie($n, $rit)
+		{
+		  if ($n < $rit){
+		  $pos = "много";
+		  }
+		  if ($n > $rit){
+		  $pos = "мало";
+		  }
+		  if ($n == $rit){
+		  $pos = "Угадал!";
+		  }
+		  return $pos;
+		}
+		
+        ?>
+     <form action="" method="post">
+ <input type="textbox" requied name="min" placeholder="минимальное загаданное">
+ <input type="textbox" requied name="max" placeholder="максимальное загаданное">
+ <input type="submit" name="submit" placeholder="сменить число">  
+ </form>
+
+	<?php
+	$n = $_COOKIE['n'];
+	if ($n == True){
+	echo $n;
+	}
+	else{
+	if ($_POST['submit'])
+        {
+         $min=$_POST['min'];
+         $max=$_POST['max'];
+		 $n = random($min, $max);
+         
+         echo $n;
+         
+         
+        }
+	}
+		
+		?>
+	</p>
+	 <form action="" method="post">
+ <input type="textbox" requied name="rit" placeholder="ваше предположение">
+ <input type="submit" name="submit1" placeholder="сменить предположение"> 
+ </form>
+	<p>
+	<?php
+		if ($_POST['submit1'])
+		{
+		 $rit=$_POST['rit'];
+		}
+		
+		
+		$pos = sravnenie($n, $rit);
+         echo $pos;
+	    
+		
+
+      ?>
+    </p>
+  </body>
+</html>
+
+
+
+
+
+
+
+
