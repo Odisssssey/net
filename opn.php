3e0625f8e23903d922b950cfb245553d3a012496
odisssssey <good_man5@mail.ru>
2016-07-11 03:39:16 +0300

    new

---
+++ opn.php
@@ -0,0 +1,17 @@
+﻿<?php
+mb_internal_encoding("utf-8");
+$f = fopen("srh.csv", "r");
+//$dat = fgetcsv($f, ",");
+while($str = fgetcsv($f))
+{
+	//var_dump($str);
+	//echo "<br/>{$str[0]}<hr/>" ;
+	$new = "new_{$str[0]}";
+	echo "<a href='img/{$str[0]}'>{$str[0]}</a><br/><img src='{$new}'><br/>";
+	
+	
+	
+
+}
+
+?>
---
+++ srh.php
@@ -0,0 +1,63 @@
+﻿<?php
+mb_internal_encoding("utf-8");
+$f = fopen("srh.csv", 'w');
+$mas = [];
+
+for($i= 1, $stp = 2; $i<$stp; $i++)
+{ 
+$file1 = "img/{$i}.jpg";
+	if(file_exists($file1))
+	{
+		echo $file1.'<br/>';
+		$ltl = [];
+		$fi = basename($file1);
+		$ltl[] = "{$fi}";
+		$stp += 1;
+		$stat = stat($file1);
+		$s = $stat['size'];
+		list($width, $height) = getimagesize($file1);
+		$pik = imagecreatetruecolor(200, 200);
+		$pic = imagecreatefromjpeg($file1);
+		imagecopyresampled($pik, $pic, 0, 0, 0, 0, 200, 200, $width, $height);
+		imagejpeg($pik, "new_{$i}.jpg", 100);		
+		
+		echo 'Размер файла: ' . $s . ' байт.<br/>';
+		$ltl[] = "{$s}"; 
+		$d = date("d.m.Y. H:i", $stat['mtime']);
+		$dt = $stat['mtime'];
+		echo 'Время изменения: ' . $d . '.<br/>';
+		$ltl[] = "{$dt}"; 
+
+		$mas["{$fi}"] = $ltl;
+		
+	}
+
+}
+
+foreach ($mas as $pt)
+{
+	fputcsv($f, $pt);
+} 
+
+var_dump($mas);
+
+fclose($f);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
