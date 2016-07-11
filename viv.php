db358cc55c4412db9f0c2aa2f2799369d22bdab0
odisssssey <good_man5@mail.ru>
2016-07-12 00:34:45 +0300

    new

---
+++ viv.php
@@ -0,0 +1,26 @@
+﻿<?php
+mb_internal_encoding("utf-8");
+$f = fopen("srh (1).csv", "r");
+echo '<h1>Жесткое сравнение с помощью === </h1> <br/>';
+echo "<table>";
+while($str = fgetcsv($f))
+{
+	echo "<tr>";
+	foreach($str as $v)
+	{
+		$vak = ";";
+		$pat = explode($vak, $v);
+			for($i=0; $i<count($pat); $i++)
+			{
+				echo "<td style= 'border: 1px solid #000'>$pat[$i]</td>";
+			}
+		
+	}
+echo "</tr>";
+
+
+}
+echo "</table>";
+
+
+
