09120a7dab1a32bc503fab82befe1e8c5a78bcb3
odisssssey <good_man5@mail.ru>
2016-07-15 16:51:07 +0300

    post/get

---
+++ action.php
@@ -0,0 +1,43 @@
+﻿<?php
+
+if ($_POST["tet"])
+	{
+	$dir = "{$_POST['tet']}";
+	echo $dir;
+	}
+	
+mb_internal_encoding("utf-8");
+$f = file_get_contents("{$dir}");
+$file = mb_convert_encoding ($f ,"UTF-8" , "Windows-1251" );
+
+$arr = json_decode($file, true);
+var_dump($arr);
+echo "<hr/><br/>";
+
+
+$s = 0;
+
+if ($_POST["submit"])
+        {
+		
+		for ($i=0; isset($_POST["rit{$i}"]); $i++)
+			{
+			$rit{$i}=$_POST["rit{$i}"];
+			//echo $rit{$i};
+			//echo $arr["{$i}"]["answer"];
+			
+            if ($rit{$i} == $arr["{$i}"]["answer"])
+            {
+             
+				$s += 1;   
+			 
+            }
+			
+			}
+		}
+		
+		echo "Вы ответили на {$s} вопросов правильно из {$i}.";
+	
+
+
+	
---
+++ actions.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+
+ if (isset($_POST['send'])){
+		
+	$box =__DIR__."/files/{$_FILES['fi']["name"]}";
+	var_dump($_FILES['fi']);
+	if(move_uploaded_file($_FILES['fi']["tmp_name"], $box))
+	{
+		echo "norm";
+	}
+
+}
+
+	
