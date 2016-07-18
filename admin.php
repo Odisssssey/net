ea21dbbd2e8a4c8ef4548ea9ec21b8b3c9935957
odisssssey <good_man5@mail.ru>
2016-07-18 03:41:27 +0300

    img

---
+++ admin.php
@@ -0,0 +1,18 @@
+﻿<html>
+  <head>
+      <meta charset="utf-8">
+  </head>
+  <body>
+
+ <form enctype="multipart/form-data" action="actions.php" method="post">
+ <input type="file" name="fi" />
+ <input type="submit" name="send" value="Загрузить" />
+ </form>
+
+ 
+ </body>
+ </html>
+ 
+ 
+ 
+ 
