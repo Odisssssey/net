19b4ef1615c38e44be42faa5db95585409188567
odisssssey <good_man5@mail.ru>
2016-08-07 12:59:09 +0300

    24

--- admin.php
+++ admin.php
@@ -1,14 +1,25 @@
-﻿<html>
+<?php
+ session_start();
+ require_once("fanctions.php"); 
+?>
+
+<html>
   <head>
       <meta charset="utf-8">
   </head>
   <body>
+    <?php 
+	if (isAdmin() == true){
+		require "header.php";
+		}else{
+		header('Location: 403.php', true); 
+	}
+	?>
 
- <form enctype="multipart/form-data" action="actions.php" method="post">
- <input type="file" name="fi" />
- <input type="submit" name="send" value="Загрузить" />
- </form>
-
+		<form enctype="multipart/form-data" action="actions.php" method="post">
+		<input type="file" name="fi" />
+		<input type="submit" name="send" value="Загрузить" />
+		</form>
  
  </body>
  </html>
