76e528560d09d8db9ad5a313c4294b3211e59547
Odisssssey <Good_man5@mail.ru>
2016-09-26 19:16:24 +0300

    Update setTema.php

--- faq/controller/setTema.php
+++ faq/controller/setTema.php
@@ -1,18 +1,17 @@
 <?php 
 require_once("../app/letter.class.php");
-//require_once("../model/model.php");
+
 ?>
 <?php 
 
 if (isset($_POST['del'])){
-//$del = delete($_POST['category']);
+
 $del = new Letter();
 $del->delTema($_POST['category']);
 
-///для записи///
+
 $use = "удалил категорию ".$_POST['category'];
 }
-///запись///
 $d = getdate();
 
 $file = fopen('../User_can.txt', 'a+');
