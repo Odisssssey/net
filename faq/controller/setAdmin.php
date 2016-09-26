be9010d4f76f88e14e5506971ca0790ef173e7fa
Odisssssey <Good_man5@mail.ru>
2016-09-26 19:15:21 +0300

    Update setAdmin.php

--- faq/controller/setAdmin.php
+++ faq/controller/setAdmin.php
@@ -5,35 +5,27 @@ require_once("user.class.php");
 
 <?php
 if(!empty($_POST['id']) && (!empty($_POST['description']))){
-//$adminSetPassword = sqlReSetPassword($_POST['id'], $_POST['description']);
 $user = new User();
 $user->reSetPassword($_POST['id'], $_POST['description']);
 
-///для записи///
 $use = " изменил пароль пользователя (".$_POST['id'].") на ".$_POST['description'];
 
 }
 
 if(!empty($_GET['id']) && ($_GET['admin'] == 1)){
-//$adminSet = sqlSetAdmin($_GET['id']);
 $user = new User();
 $user->setAdmin($_GET['id']);
 
-///для записи///
 $use = " сделал пользователя (".$_GET['id'].") админом";
 
 }
 
 if(!empty($_GET['id']) && ($_GET['admin'] == 0)){
-//$adminDel = sqlDelAdmin($_GET['id']);
 $user = new User();
 $user->delAdmin($_GET['id']);
 
-///для записи///
 $use = " убрал пользователя (".$_GET['id'].") из админки";
 }
-
-///запись///
 $d = getdate();
 
 $file = fopen('../User_can.txt', 'a+');
