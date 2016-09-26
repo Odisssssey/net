f43f69b46d24fc7ac1e8db25957621d9534f251c
Odisssssey <Good_man5@mail.ru>
2016-09-26 19:15:53 +0300

    Update setQuestion.php

--- faq/controller/setQuestion.php
+++ faq/controller/setQuestion.php
@@ -1,11 +1,9 @@
 <?php 
 require_once("../app/letter.class.php");
-//require_once("../model/model.php");
 ?>
 <?php 
 
 if(isset($_POST['delquestion'])){
-	//$question = delQuestion($_POST['id']);
 	$question = new Letter();
 	$question->delQuestion($_POST['id']);
 	
@@ -14,7 +12,6 @@ if(isset($_POST['delquestion'])){
 }
 
 if(isset($_POST['chengequestion'])){
-	//$chengeQuestion = sqlChengeQuestion($_POST['id']);
 	$question = new Letter();
 	$question->sqlChengeQuestion($_POST['id']);
 	
@@ -22,7 +19,6 @@ if(isset($_POST['chengequestion'])){
 }
 
 if(isset($_POST['setquestion'])){
-	//$chengeQuestion = sqlSetQuestion($_POST['id']);
 	$question = new Letter();
 	$question->sqlSetQuestion($_POST['id']);
 	
@@ -35,21 +31,18 @@ if(isset($_POST['user'])){
 	$use = ' изменил (';
 	
 	if(isset($user['name'])){
-		//$setQuestionInAdmin = sqlSetQuestionInAdmin("name", $user['name'], $_POST['id']);
 		$question = new Letter();
 		$question->setQuestionInAdmin("name", $user['name'], $_POST['id']);
 		
 		$use .= 'имя ';
 	}
 	if(isset($user['mail'])){
-		//$setQuestionInAdmin = sqlSetQuestionInAdmin("mail", $user['mail'], $_POST['id']);
 		$question = new Letter();
 		$question->setQuestionInAdmin("mail", $user['mail'], $_POST['id']);
 		
 		$use .= 'почту ';
 	}
 	if(isset($user['mesage'])){
-		//$setQuestionInAdmin = sqlSetQuestionInAdmin("description", $user['mesage'], $_POST['id']);
 		$question = new Letter();
 		$question->setQuestionInAdmin("description", $user['mesage'], $_POST['id']);
 		if ($question->proverkaNaTelegram($user['mail']) == true){
@@ -59,7 +52,6 @@ if(isset($_POST['user'])){
 		$use .= 'вопрос ';
 	}
 	if(isset($user['answer'])){
-		//$setQuestionInAdmin = sqlSetQuestionInAdmin("answer", $user['answer'], $_POST['id']);
 		$question = new Letter();
 		$question->setQuestionInAdmin("answer", $user['answer'], $_POST['id']);
 		if(!empty($_POST['ban'])){
@@ -72,7 +64,6 @@ if(isset($_POST['user'])){
 	$use .= ')в вопросе ('.$_POST['id'].")";
 }
 
-///запись///
 $d = getdate();
 
 $file = fopen('../User_can.txt', 'a+');
